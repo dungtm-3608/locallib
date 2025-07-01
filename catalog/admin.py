1504cea78e10af07a3726f7913225d4b3551a93d
dncmaduro <manhdungtran.vnuuet@gmail.com>
2025-07-01 15:11:44 +0700

    feat: part 4

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Author, Book, Genre, BookInstance
 
 # Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': ('book', 'imprint', 'id')
+            }
+        ),
+        (
+            'Availability',
+            {
+                'fields': ('status', 'due_back')
+            }
+        )
+    )
